c0e4bdccbdcaf7896863e5f43a216fc3e6ca235a
Rishabh Singh <itsindrajput@gmail.com>
2023-09-20 19:21:06 +0530

    Fixed Some Issues

--- Lecture3(LayingTheFoundation)/App.js
+++ Lecture3(LayingTheFoundation)/App.js
@@ -1,38 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-/*
-One way to write code in React Is using React.createElement()
-*/
 
-const heading=React.createElement("div",
-    {id:"heading",key:"100"},
-    ["Laying The Foundation !", React.createElement("ul",{},
-    [React.createElement("li",{id:"header"},"About"),
-    React.createElement("li",{id:"header"},"Support"),
-    React.createElement("li",{id:"header"},"Home")])]);
-    
-/*
-Other way to write code in React Is using JSX
-*/
-
-    const navbar=<h1>Hello!</h1>
+const Title=()=>(
+    <h1 className="title" key="h1">Chapter - 03</h1>
+);
 
 /*
 Functional Components
 */
-// const HeaderComponent=()=>{
-//     // return is optional here, but it's good practice to have one
-//     return(<h1>
-//         <h1>Laying The Foundation !</h1>
-//         <ul>
-//             <li>Home</li>
-//             <li>About</li>
-//             <li>Support</li>
-//         </ul>
-//     </h1>
-//     );
-// };
+const HeaderComponents=()=>{
+    // return is optional here, but it's good practice to have one
+    return(<h1>
+        <h1 className="title">Laying The Foundation !</h1>
+        <Title />
+        <ul>
+            <li className="heading"><img src="https://unsplash.com/photos/_Fqoswmdmoo"></img></li>
+            <li className="heading">Home</li>
+            <li className="heading">About</li>
+            <li className="heading">Support</li>
+        </ul>
+    </h1>
+    );
+};
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeaderComponents />);
